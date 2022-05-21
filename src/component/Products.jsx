82ca74dd22ca6aec07a427759e86d6783df08f68
Sannasichottu <sannasichottu@gmail.com>
2022-05-21 15:35:43 +0530

    empty

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -19,6 +19,7 @@ const Products = () => {
         console.log(filter);
       }
       return () => {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
         componentMounted = false;
       };
     };
