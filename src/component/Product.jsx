b7eebbf4575eacb6a68659aacc6485c751a3925c
Sannasichottu <sannasichottu@gmail.com>
2022-05-21 15:25:39 +0530

    netlify upload

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -24,6 +24,7 @@ const Product = () => {
             setLoading(false);
         }
         getProduct();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     },[]);
 
     const Loading = () => {
